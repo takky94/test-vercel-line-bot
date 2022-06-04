d8d652d9464b294a2ed0457fe1cd51f18a42c4f3
t-94 <infourbh@gmail.com>
2022-06-04 21:41:33 +0900

    feature: liffを試し起動

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,7 @@
 export const ENV = {
+  // server
   LINE_CHANNEL_ACCESS_TOKEN: process.env.LINE_CHANNEL_ACCESS_TOKEN || "",
   LINE_CHANNEL_SECRET: process.env.LINE_CHANNEL_SECRET || "",
+  // client
+  LINE_LIFF_ID: process.env.NEXT_PUBLIC_LINE_LIFF_ID || "",
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import { useEffect, useState } from "react";
+import { ENV } from "../env";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [liffObject, setLiffObject] = useState<any>(null);
+
+  useEffect(() => {
+    import("@line/liff").then((liff: any) => {
+      liff
+        .init({ liffId: ENV.LINE_LIFF_ID })
+        .then(() => {
+          setLiffObject(liff);
+          if (liff.isLoggedIn()) {
+            // ログインの確認を取れたら
+          }
+        })
+        .catch((err: any) => {
+          console.error({ err });
+        });
+    });
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
